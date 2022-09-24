824d8c684ae0e579045797fe4f049678450a997b
Sensei <wellsm488@gmail.com>
2022-09-24 16:57:31 +0100

    Changed LocatableTrace and Arucas.runSafe

--- src/main/kotlin/me/senseiwells/arucas/core/Arucas.kt
+++ src/main/kotlin/me/senseiwells/arucas/core/Arucas.kt
@@ -1,7 +1,7 @@
 package me.senseiwells.arucas.core
 
 import me.senseiwells.arucas.classes.ClassInstance
-import me.senseiwells.arucas.exceptions.Propagator
+import me.senseiwells.arucas.exceptions.*
 import me.senseiwells.arucas.nodes.Statement
 import java.nio.file.Path
 
@@ -10,6 +10,23 @@ object Arucas {
     val VERSION = "2.0.0"
     @JvmStatic
     val PATH: Path = Path.of(System.getProperty("user.home")).resolve(".arucas")
+
+    @JvmStatic
+    fun runSafe(interpreter: Interpreter): ClassInstance {
+        return try {
+            run(interpreter)
+        } catch (e: RuntimeError) {
+            throw e
+        } catch (f: FatalError) {
+            // Fatal errors shouldn't be handled by
+            // the parent as this
+            throw f
+        } catch (e: CompileError) {
+            // Compile errors are propagated as runtime errors
+            // so parent script can handle accordingly
+            runtimeError("Compiling failed", e)
+        }
+    }
 
     @JvmStatic
     fun run(interpreter: Interpreter): ClassInstance {
--- src/main/kotlin/me/senseiwells/arucas/exceptions/CompileError.kt
+++ src/main/kotlin/me/senseiwells/arucas/exceptions/CompileError.kt
@@ -1,14 +1,14 @@
 package me.senseiwells.arucas.exceptions
 
 import me.senseiwells.arucas.core.Interpreter
-import me.senseiwells.arucas.utils.Trace
+import me.senseiwells.arucas.utils.LocatableTrace
 import me.senseiwells.arucas.utils.error.ErrorUtils
 
-fun compileError(details: String, trace: Trace): Nothing = throw CompileError(details, trace)
+fun compileError(details: String, trace: LocatableTrace): Nothing = throw CompileError(details, trace)
 
 class CompileError(
     details: String,
-    val trace: Trace
+    val trace: LocatableTrace
 ): ArucasError(details) {
     override fun format(interpreter: Interpreter): String {
         return "> ${this.trace}\nCompilation Error: ${ErrorUtils.format(this.trace, interpreter, this.message) ?: this.message}"
